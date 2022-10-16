24a59ebdc2a86ba9e719984d714efce97d652c16
Narvik <narvik.vesangi@gmail.com>
2022-10-16 19:33:42 +0530

    Initial commit

---
+++ Soldier.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+public class Soldier : Node2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    public int state = 0;
+    public bool right = true;
+    // var sprite = GetNode<Sprite>("Position2D/Sprite");
+
+    public int down_frame = 8;
+    [Export]
+    public int right_frame = 6;
+    [Export]
+    public int level = 15;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        var sprite = GetNode<Sprite>("Position2D/Sprite");
+        sprite.Frame = 6;
+    }
+
+    public void OnButtonPressed()
+    {
+        if (state == 0)
+        {
+            var sprite = GetNode<Sprite>("Position2D/Sprite");
+            // sprite.Frame = 6;
+            sprite.FlipH = !sprite.FlipH;
+            right = !right;
+            string dir = (right) ? "R" : "L";
+            GD.Print("Soldier Clicked!!!");
+            GD.Print("Current Direction " + dir);
+        }
+    }
+
+    // //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    // public override void _Process(float delta)
+    // {
+
+    // }
+}
---
+++ TileMap.cs
@@ -0,0 +1,47 @@
+using Godot;
+using System;
+
+public class TileMap : Godot.TileMap
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    TileSet _tileset;
+    [Export]
+    public int Rows = 18;
+    [Export]
+    public int Columns = 18;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        // _tileset = GetTileset();
+        int temp_x = Rows / 2 + 1;
+        int temp_y = Columns / 2 + 1;
+        for (int i = -temp_x; i <= temp_x; i++)
+        {
+            for (int j = -temp_y; j <= temp_y; j++)
+            {
+                if (i == -temp_x || i == temp_x || j == -temp_y || j == temp_y)
+                {
+                    SetCell(i, j, TileSet.FindTileByName("borderGrass"));
+                }
+                else if (j == -temp_y + 1)
+                {
+                    SetCell(i, j, TileSet.FindTileByName("waterTile"));
+
+                }
+                else
+                {
+                    SetCell(i, j, TileSet.FindTileByName("grassTile"));
+                }
+            }
+        }
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+}
